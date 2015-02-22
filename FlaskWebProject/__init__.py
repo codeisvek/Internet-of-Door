fd3350b71344ae1ca5cf33095ad16a93fb38f570
David Bui <david@dtbui.com>
2015-02-21 18:51:58 -0800

    doorbell ringing

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask, request, redirect
 from twilio.rest import TwilioRestClient
  
 # Your Account Sid and Auth Token from twilio.com/user/account
-account_sid = "AC32a3c49700934481addd5ce1659f04d2"
+account_sid = "ACb490a8d83b1f8becd4363e27c4e8352b"
 auth_token  = "9702a9d7f685c995868eaee46495c792"
 client = TwilioRestClient(account_sid, auth_token)
  
@@ -12,7 +12,15 @@ application = 0
 
 @app.route('/')
 def hello_world():
-    message = client.messages.create(body="Jenny please?! I love you <3",
+    temp = application
+    if application != 0:
+        global application
+        application = 0   
+    return str(temp)
+
+@app.route('/door')
+def ring():
+    message = client.messages.create(body="Someone's at the door. Let them in?",
     to="+17149473284",    # Replace with your phone number
     from_="15624441271") # Replace with your Twilio number
     print message.sid
